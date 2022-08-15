3ddcb2f66c0d8dbaf9569b3b8a3133c5e1e57096
Sakyuba Noe <398noe@gmail.com>
2022-08-15 17:31:48 +0900

    Challenged station 13〜15

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,22 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
+  const result = await test(userList)
   return await result
 }
 
-function test() {
-  return
+function buildFullName(data) {
+  return {
+      ...data,
+      full_name: data.family_name + " " + data.first_name,
+  }
 }
 
+const sleep = () => new Promise(resolve => setTimeout(resolve, 3000));
+
+async function test(userList) {
+  // 3秒待つ
+  await sleep();
+  // ユーザデータを返す
+  return userList.map(buildFullName);
+}
